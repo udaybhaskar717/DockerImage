3ff0b0299b1a725c4811066faacabcf657b78eeb
udaybhaskar717 <udaybhaskar717@gmail.com>
2025-01-24 09:54:25 +0530

    Again Updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
+import sys
+import os
+
+# Add the parent directory to the sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
+
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
